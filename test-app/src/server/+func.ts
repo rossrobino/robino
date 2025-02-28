eeaecbdb8cdf6860710e0b976bd148422f2e1ae8
rossrobino <rbrobino@gmail.com>
2025-02-28 17:21:16 -0500

    comment

--- test-app/src/server/+func.ts
+++ test-app/src/server/+func.ts
@@ -79,143 +79,132 @@ app.get("/chat", async (c) => {
 
 export const handler = app.fetch;
 
-const md = `\n\n---
+// const md = `\n\n---
 
-# h1 Heading
-## h2 Heading
-### h3 Heading
-#### h4 Heading
-##### h5 Heading
-###### h6 Heading
+// # h1 Heading
+// ## h2 Heading
+// ### h3 Heading
+// #### h4 Heading
+// ##### h5 Heading
+// ###### h6 Heading
 
+// ## Horizontal Rules
 
-## Horizontal Rules
+// ___
 
-___
+// ---
 
----
+// ***
 
-***
+// ## Typographic replacements
 
+// Enable typographer option to see result.
 
-## Typographic replacements
+// (c) (C) (r) (R) (tm) (TM) (p) (P) +-
 
-Enable typographer option to see result.
+// test.. test... test..... test?..... test!....
 
-(c) (C) (r) (R) (tm) (TM) (p) (P) +-
+// !!!!!! ???? ,,  -- ---
 
-test.. test... test..... test?..... test!....
+// "Smartypants, double quotes" and 'single quotes'
 
-!!!!!! ???? ,,  -- ---
+// ## Emphasis
 
-"Smartypants, double quotes" and 'single quotes'
+// **This is bold text**
 
+// __This is bold text__
 
-## Emphasis
+// *This is italic text*
 
-**This is bold text**
+// _This is italic text_
 
-__This is bold text__
+// ~~Strikethrough~~
 
-*This is italic text*
+// ## Blockquotes
 
-_This is italic text_
+// > Blockquotes can also be nested...
+// >> ...by using additional greater-than signs right next to each other...
+// > > > ...or with spaces between arrows.
 
-~~Strikethrough~~
+// ## Lists
 
+// Unordered
 
-## Blockquotes
+// + Create a list by starting a line with \`+\`, \`-\`, or \`*\`
+// + Sub-lists are made by indenting 2 spaces:
+//   - Marker character change forces new list start:
+//     * Ac tristique libero volutpat at
+//     + Facilisis in pretium nisl aliquet
+//     - Nulla volutpat aliquam velit
+// + Very easy!
 
+// Ordered
 
-> Blockquotes can also be nested...
->> ...by using additional greater-than signs right next to each other...
-> > > ...or with spaces between arrows.
+// 1. Lorem ipsum dolor sit amet
+// 2. Consectetur adipiscing elit
+// 3. Integer molestie lorem at massa
 
+// 1. You can use sequential numbers...
+// 1. ...or keep all the numbers as \`1.\`
 
-## Lists
+// Start numbering with offset:
 
-Unordered
+// 57. foo
+// 1. bar
 
-+ Create a list by starting a line with \`+\`, \`-\`, or \`*\`
-+ Sub-lists are made by indenting 2 spaces:
-  - Marker character change forces new list start:
-    * Ac tristique libero volutpat at
-    + Facilisis in pretium nisl aliquet
-    - Nulla volutpat aliquam velit
-+ Very easy!
+// ## Code
 
-Ordered
+// Inline \`code\`
 
-1. Lorem ipsum dolor sit amet
-2. Consectetur adipiscing elit
-3. Integer molestie lorem at massa
+// Indented code
 
+//     // Some comments
+//     line 1 of code
+//     line 2 of code
+//     line 3 of code
 
-1. You can use sequential numbers...
-1. ...or keep all the numbers as \`1.\`
+// Block code "fences"
 
-Start numbering with offset:
+// \`\`\`
+// Sample text here...
+// \`\`\`
 
-57. foo
-1. bar
+// Syntax highlighting
 
+// \`\`\` js
+// var foo = function (bar) {
+//   return bar++;
+// };
 
-## Code
+// console.log(foo(5));
+// \`\`\`
 
-Inline \`code\`
+// ## Tables
 
-Indented code
+// | Option | Description |
+// | ------ | ----------- |
+// | data   | path to data files to supply the data that will be passed into templates. |
+// | engine | engine to be used for processing templates. Handlebars is the default. |
+// | ext    | extension to be used for dest files. |
 
-    // Some comments
-    line 1 of code
-    line 2 of code
-    line 3 of code
+// Right aligned columns
 
+// | Option | Description |
+// | ------:| -----------:|
+// | data   | path to data files to supply the data that will be passed into templates. |
+// | engine | engine to be used for processing templates. Handlebars is the default. |
+// | ext    | extension to be used for dest files. |
 
-Block code "fences"
+// ## Links
 
-\`\`\`
-Sample text here...
-\`\`\`
+// [link text](http://dev.nodeca.com)
 
-Syntax highlighting
+// [link with title](http://nodeca.github.io/pica/demo/ "title text!")
 
-\`\`\` js
-var foo = function (bar) {
-  return bar++;
-};
+// Autoconverted link https://github.com/nodeca/pica (enable linkify to see)
 
-console.log(foo(5));
-\`\`\`
+// ## Images
 
-## Tables
-
-| Option | Description |
-| ------ | ----------- |
-| data   | path to data files to supply the data that will be passed into templates. |
-| engine | engine to be used for processing templates. Handlebars is the default. |
-| ext    | extension to be used for dest files. |
-
-Right aligned columns
-
-| Option | Description |
-| ------:| -----------:|
-| data   | path to data files to supply the data that will be passed into templates. |
-| engine | engine to be used for processing templates. Handlebars is the default. |
-| ext    | extension to be used for dest files. |
-
-
-## Links
-
-[link text](http://dev.nodeca.com)
-
-[link with title](http://nodeca.github.io/pica/demo/ "title text!")
-
-Autoconverted link https://github.com/nodeca/pica (enable linkify to see)
-
-
-## Images
-
-![Minion](https://octodex.github.com/images/minion.png)
-![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
-`;
+// ![Minion](https://octodex.github.com/images/minion.png)
+// ![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
+// `;
