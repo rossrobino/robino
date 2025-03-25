a3f88d24173abd1a08fb2fcd88ba6474c0f71164
rossrobino <rbrobino@gmail.com>
2025-03-25 11:56:39 -0400

    deprecate html package

--- packages/html/src/index.ts
+++ packages/html/src/index.ts
@@ -1,3 +1,5 @@
-export { escape } from "./escape/index.js";
-export { Page } from "./page/index.js";
-export type * from "./types/index.js";
+console.warn(
+	"@robino/html package has been deprecated and moved into @robino/jsx",
+);
+
+export {};
