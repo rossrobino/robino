673b039622bb5440a41cf93d872094abc485504d
rossrobino <rbrobino@gmail.com>
2025-05-02 13:57:37 -0400

    refactor

--- packages/md/src/table-overflow/index.ts
+++ packages/md/src/table-overflow/index.ts
@@ -1,34 +1,19 @@
-import MarkdownIt from "markdown-it";
 import type { PluginSimple } from "markdown-it";
 import type { RenderRule } from "markdown-it/lib/renderer.mjs";
 
 /**
  * Markdown-it plugin that wraps table elements in a container with horizontal overflow.
- *
- * @param md markdown-it instance
  */
-export const tableOverflow: PluginSimple = (md: MarkdownIt) => {
-	// Store reference to the original renderer rules
-	const originalTableOpenRenderer: RenderRule =
-		md.renderer.rules.table_open ??
-		((tokens, idx, options, _env, self) =>
-			self.renderToken(tokens, idx, options));
+export const tableOverflow: PluginSimple = (mdIt) => {
+	const proxy: RenderRule = (tokens, i, options, _env, self) =>
+		self.renderToken(tokens, i, options);
 
-	const originalTableCloseRenderer: RenderRule =
-		md.renderer.rules.table_close ??
-		((tokens, idx, options, _env, self) =>
-			self.renderToken(tokens, idx, options));
+	const originalTableOpen = mdIt.renderer.rules.table_open ?? proxy;
+	const originalTableClose = mdIt.renderer.rules.table_close ?? proxy;
 
-	md.renderer.rules.table_open = (tokens, idx, options, env, self) => {
-		return (
-			`<div style="overflow-x: auto;">` +
-			originalTableOpenRenderer(tokens, idx, options, env, self)
-		);
-	};
+	mdIt.renderer.rules.table_open = (...args) =>
+		`<div style="overflow-x: auto">` + originalTableOpen(...args);
 
-	md.renderer.rules.table_close = (tokens, idx: number, options, env, self) => {
-		return (
-			originalTableCloseRenderer(tokens, idx, options, env, self) + "</div>"
-		);
-	};
+	mdIt.renderer.rules.table_close = (...args) =>
+		originalTableClose(...args) + "</div>";
 };
